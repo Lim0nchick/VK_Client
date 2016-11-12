4ef0be89145409ceee6f19d7a5bf1649af602020
Lim0nchick <Lim0nchick@yandex.ru>
2016-11-12 19:31:59 +0400

    Update main.cpp
    Hide token :)

--- examples/main.cpp
+++ examples/main.cpp
@@ -14,9 +14,9 @@ using namespace std;
 int main()
 {
 <<<<<<< HEAD:examples/main1.cpp
-    string access_token="713052e147c58cd93b85a180b76561f0225b4c84f91af051cf411d641c25484a4d14ad0b68b9eb113bf87";
-    //cout << "Access_token: ";
-    //cin >> access_token;
+    string access_token;
+    cout << "Access_token: ";
+    cin >> access_token;
     VK::VK_Client data({{"token", access_token }});
     data.check_connection();
 =======
