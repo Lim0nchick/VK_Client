12e01d885a4db0d6047a4a30f91909cd18adef5d
Lim0nchick <Lim0nchick@yandex.ru>
2016-11-12 18:24:41 +0300

    1st Stage (ver. 0.1)

--- examples/main1.cpp
+++ examples/main1.cpp
@@ -1,52 +1,18 @@
 //
 // Created by root on 10/30/16.
 //
-#include <curl/curl.h>
+
 #include <iostream>
-#include <clocale>
+#include <map>
+#include "client.hpp"
+#include <string>
 using namespace std;
-int main ()
+int main()
 {
-    setlocale(0, "1251");
-    CURL *curl;
-    CURLcode res;
-    long response_code;
-    curl_global_init(CURL_GLOBAL_DEFAULT);
-    curl = curl_easy_init();
-    curl_easy_setopt(curl, CURLOPT_USERAGENT, "Mozilla/5.0 (X11; Linux amd64; rv:14.0) Gecko/20100101 Firefox/14.0.1");
-    if (curl)
-    {
-
-        curl_easy_setopt(curl, CURLOPT_URL, "https://vk.com");
-        curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &response_code);
-        cout << response_code << endl;
-        res = curl_easy_perform(curl);
-        if (res != CURLE_OK) cout << "curl_easy_perform() failed:" << curl_easy_strerror(res) << endl;
-        cout << "\n\n\n\n";
-
-
-        curl_easy_setopt(curl, CURLOPT_USERPWD, "[Lim0nchick@yandex.ru]:[11797277359LImo]");
-        curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1);cout << endl;
-        res = curl_easy_perform(curl);
-        if (res != CURLE_OK) cout << "curl_easy_perform() failed:" << curl_easy_strerror(res) << endl;
-        curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &response_code);
-        cout << response_code << endl;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        curl_easy_cleanup(curl);
-    }
-    curl_global_cleanup();
+    string access_token="713052e147c58cd93b85a180b76561f0225b4c84f91af051cf411d641c25484a4d14ad0b68b9eb113bf87";
+    //cout << "Access_token: ";
+    //cin >> access_token;
+    VK::VK_Client data({{"token", access_token }});
+    data.check_connection();
     return 0;
 }
--- sources/client.cpp
+++ sources/client.cpp
@@ -2,4 +2,41 @@
 // Created by root on 10/30/16.
 //
 
-#include "../include/VK/client.hpp"
+#include <iostream>
+#include <curl/curl.h>
+#include "client.hpp"
+using namespace std;
+namespace VK
+{
+
+    auto VK_Client::check_connection()-> bool
+    {
+
+        CURL *curl = curl_easy_init();
+        if (curl)
+        {
+            string data_to_send = "access_token=713052e147c58cd93b85a180b76561f0225b4c84f91af051cf411d641c25484a4d14ad0b68b9eb113bf87&v=5.59";
+            CURLcode res;
+            curl_easy_setopt(curl, CURLOPT_URL, "https://api.vk.com/method/users.get?");
+            curl_easy_setopt(curl, CURLOPT_POSTFIELDS, data_to_send.c_str());
+
+            long response_code;
+            res = curl_easy_perform(curl);
+            if (res == CURLE_OK)
+            {
+                long response_code;
+                curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &response_code);
+		cout << endl;
+		if (response_code>=300 && response_code<400)
+			curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1);
+		if (response_code >=400)
+			cout << "Error connection: " << response_code << endl;
+                if (response_code >= 200 && response_code < 300)
+	                cout << "Connected successfully" << endl;
+            }
+
+
+        }
+        curl_easy_cleanup(curl);
+    }
+};
